4be770403bc9be2096057171eacf590937fbb6a3
zztnit <zztnit@gmail.com>
2015-07-16 12:06:15 +0800

    learn how to use swirl
    swirl is a lib for learing R

---
+++ Swirlstudent.R
@@ -0,0 +1,14 @@
+install.packages("swirl")
+library("swirl")#每次进入R后要run一次。
+
+swirl()
+
+zinstall_from_swirl("Data Analysis")
+swirl()
+
+install_from_swirl("Data Analysis")
+
+install.packages("dplyr")#当swirl不能正确安装dplyr时，运行该命令。
+library("dplyr")#每次进入R后要run一次。
+
+library(dplyr)
